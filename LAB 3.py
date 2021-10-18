d4e6c57ccaf8d531f34c3167ac6a52d1a8547036
DripTill <91988627+DripTill@users.noreply.github.com>
2021-10-17 21:41:00 -0500

    Update LAB 3.py

--- LAB 3.py
+++ LAB 3.py
@@ -29,5 +29,9 @@ fahrenheit = round(fahrenheit,2)
 print (celsius,'celsius is: ',fahrenheit, 'fahrenheit ')
 
 #Problem 7 - It is possible to name the days 0 through 6 where day 0 is Sunday and day 6 is Saturday. If you go on a wonderful holiday leaving on day number 3 (a Wednesday) and you return home after 10 nights you would return home on a Saturday (day 6) Write a general version of the program which asks for the starting day number, and the length of your stay, and it will tell you the number of day of the week you will return on.
-                
+      start_day = int(input(" what day is 0-6"))
+days_to_wait = int(input("how many days of vacation? "))
+end_day = (start_day + days_to_wait)
+print (end_day)
+          
 
